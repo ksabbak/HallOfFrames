5cf9b4319440d2d3a116a50e2aedc389122a5340
K Sabbak <ksabbak@hogwarts-2.local>
2016-01-20 14:23:01 -0600

    Initial Commit

---
+++ HallOfFrames/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HallOfFrames
+//
+//  Created by K Sabbak on 1/20/16.
+//  Copyright © 2016 mm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HallOfFrames/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HallOfFrames
+//
+//  Created by K Sabbak on 1/20/16.
+//  Copyright © 2016 mm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
